a93b1341d91a11f683bdea4505c09a0e5c41a158
Josh Gaber <joshgaber@gmail.com>
2019-12-08 17:13:58 -0500

    Day 8 part 2 complete

--- day8/index.js
+++ day8/index.js
@@ -1,15 +1,15 @@
 const fs = require('fs')
 
 fs.readFile('./input.txt', 'utf8', function(err, data) {
-    let layers = data.match(/.{1,150}/g);
+    let layers = data.match(/.{1,150}/g).map(x => x.split("").map(Number));
 
-    let lowest0 = layers.reduce((lowest, layer) => {
-        let layerList = layer.split("").map(Number);
-        let count0 = layerList.filter(x => x === 0).length;
-        let count1 = layerList.filter(x => x === 1).length;
-        let count2 = layerList.filter(x => x === 2).length;
-        return (lowest.zeroes < count0 ? lowest : {zeroes: count0, '1x2': count1*count2})
-    }, {'zeroes': Infinity})
+    let message = [];    
 
-    console.log(lowest0)
+    for (i in layers[0]) {
+        message[i] = layers.reduce((shown, layer) => (shown === 2 ? layer[i] : shown), 2);
+    }
+
+    for (let i = 0; i<6; i++) {
+        console.log(message.slice(0+25*i,24+25*i).join('').replace(/0/g, ' ').replace(/1/g, '\u25A0'));
+    }
 })
