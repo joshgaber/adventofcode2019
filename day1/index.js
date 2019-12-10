97eb2cc584b01bc7a7fa5e136ca3d516b30648a6
Josh Gaber <joshgaber@gmail.com>
2019-12-10 16:48:40 -0500

    Show results for both part 1 and part 2

--- day1/index.js
+++ day1/index.js
@@ -1,14 +1,22 @@
 const fs = require('fs');
 
 const calculateFuel = function(data) {
-    let fuel = Math.floor(parseInt(data) / 3) - 2;
-    return Math.max(0, fuel) + (fuel > 0 ? calculateFuel(fuel) : 0);
-}
+    return Math.max(0, Math.floor(parseInt(data) / 3) - 2);
+};
+
+const calculateWeightOfFuel = function(data) {
+    let fuel = calculateFuel(data);
+    return Math.max(0, fuel) + (fuel > 0 ? calculateWeightOfFuel(fuel) : 0);
+};
 
 fs.readFile('./input.txt', {encoding: 'utf-8'}, (err, data) => {
-    
     const modules = data.split(/\r?\n/g);
-    const transformed = modules.map(calculateFuel);
+
+    let transformed = modules.map(calculateFuel);
+    let total = transformed.reduce((a, b) => a + b, 0);
+    console.log('Total weight of fuel needed (base):', total);
+
+    transformed = modules.map(calculateWeightOfFuel);
     total = transformed.reduce((a, b) => a + b, 0);
-    console.log(total);
+    console.log('Total weight of fuel needed (withAddedFuel):', total);
 });
