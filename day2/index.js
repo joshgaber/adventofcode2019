df523eb3f6d36268fdc6dbd7f50415d6b6a81f48
Josh Gaber <joshgaber@gmail.com>
2019-12-10 17:01:50 -0500

    Show results for both part 1 and part 2

--- day2/index.js
+++ day2/index.js
@@ -2,7 +2,25 @@ const fs = require('fs');
 const goal = 19690720;
 
 fs.readFile('./input.txt', 'utf8', function(err, data) {
-    let commands = data.split(',').map(data => parseInt(data));
+    const commands = data.split(',').map(Number);
+
+    let newCommands = commands.slice();
+    newCommands[1] = 12;
+    newCommands[2] = 2;
+    let position = 0;
+
+    do {
+        let command = newCommands[position];
+        if (command === 99) break;
+        if (command === 1) {
+            newCommands[newCommands[position + 3]] = newCommands[newCommands[position + 1]] + newCommands[newCommands[position + 2]];
+        } else if (command === 2) {
+            newCommands[newCommands[position + 3]] = newCommands[newCommands[position + 1]] * newCommands[newCommands[position + 2]];
+        }
+        position += 4;
+    } while (true);
+
+    console.log('Gravity assist reset code:', newCommands[0]);
 
     for(let i = 0; i < 100; i++) {
         for (let j = 0; j < 100; j++) {
@@ -23,7 +41,7 @@ fs.readFile('./input.txt', 'utf8', function(err, data) {
             } while (true);
 
             if (newCommands[0] === goal) {
-                console.log(`noun=${i} verb=${j}`);
+                console.log(`noun=${i} verb=${j}; input:`, i * 100 + j);
             }
         }
     }
@@ -40,4 +58,4 @@ fs.readFile('./input.txt', 'utf8', function(err, data) {
     // } while (true);
 
     // console.log(commands);
-})
+});
