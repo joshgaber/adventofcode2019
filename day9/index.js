1676637b1d4695cc54635006f210739d1f6c4d91
Josh Gaber <joshgaber@gmail.com>
2019-12-11 16:24:51 -0500

    Read file synchronously. Can be run from anywhere.

--- day9/index.js
+++ day9/index.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
+const path = require('path');
 const Intcode = require('./intcode');
 
-fs.readFile('./input.txt', 'utf8', function(err, data) {
-    const memory = data.split(',').map(Number);
-    let machine1 = new Intcode(memory, [1]);
+data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
+const memory = data.split(',').map(Number);
+let machine1 = new Intcode(memory, [1]);
 
-    machine1.run();
-    console.log('BOOST Keycode', machine1.lastOutput);
+machine1.run();
+console.log('BOOST Keycode', machine1.lastOutput);
 
-    let machine2 = new Intcode(memory, [2]);
+let machine2 = new Intcode(memory, [2]);
 
-    machine2.run();
-    console.log('Distress Signal Coordinates', machine2.lastOutput);
-})
+machine2.run();
+console.log('Distress Signal Coordinates', machine2.lastOutput);
