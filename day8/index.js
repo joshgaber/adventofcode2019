a38a56705bc9483c5c3b15d11cc98595d9c2f34e
Josh Gaber <joshgaber@gmail.com>
2019-12-11 16:22:04 -0500

    Run both parts together.

--- day8/index.js
+++ day8/index.js
@@ -1,15 +1,21 @@
-const fs = require('fs')
+const fs = require('fs');
+const path = require('path');
 
-fs.readFile('./input.txt', 'utf8', function(err, data) {
-    let layers = data.match(/.{1,150}/g).map(x => x.split("").map(Number));
+const numberCount = (layer, digit) => layer.filter(x => x === digit).length;
 
-    let message = [];    
+data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
+let layers = data.match(/.{1,150}/g).map(x => x.split("").map(Number));
 
-    for (i in layers[0]) {
-        message[i] = layers.reduce((shown, layer) => (shown === 2 ? layer[i] : shown), 2);
-    }
+fewestZeroLayer = layers.reduce((a, b) => numberCount(a, 0) < numberCount(b, 0) ? a : b, layers[0]);
+console.log(`count of ones x count of twos in fewest zero layer: `, numberCount(fewestZeroLayer, 1) * numberCount(fewestZeroLayer, 2));
+console.log();
 
-    for (let i = 0; i<6; i++) {
-        console.log(message.slice(0+25*i,24+25*i).join('').replace(/0/g, ' ').replace(/1/g, '\u25A0'));
-    }
-})
+let message = [];
+for (const i in layers[0]) {
+    message[i] = layers.reduce((shown, layer) => (shown === 2 ? layer[i] : shown), 2);
+}
+
+for (let i = 0; i<6; i++) {
+    console.log(message.slice(25*i, 24+25*i).join('')
+        .replace(/0/g, ' ').replace(/1/g, '\u2588'));
+}
