6d73cdc76d21b9877c93e63f40f8118f062b510c
Josh Gaber <joshgaber@gmail.com>
2019-12-11 15:42:30 -0500

    File read synchronously. Index.js can be run from anywhere.

--- day1/index.js
+++ day1/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path');
 
 const calculateFuel = function(data) {
     return Math.max(0, Math.floor(parseInt(data) / 3) - 2);
@@ -9,14 +10,13 @@ const calculateWeightOfFuel = function(data) {
     return Math.max(0, fuel) + (fuel > 0 ? calculateWeightOfFuel(fuel) : 0);
 };
 
-fs.readFile('./input.txt', {encoding: 'utf-8'}, (err, data) => {
-    const modules = data.split(/\r?\n/g);
+const data = fs.readFileSync(path.join(__dirname, './input.txt'), 'utf8');
+const modules = data.split(/\r?\n/g);
 
-    let transformed = modules.map(calculateFuel);
-    let total = transformed.reduce((a, b) => a + b, 0);
-    console.log('Total weight of fuel needed (base):', total);
+let transformed = modules.map(calculateFuel);
+let total = transformed.reduce((a, b) => a + b, 0);
+console.log('Total weight of fuel needed (base):', total);
 
-    transformed = modules.map(calculateWeightOfFuel);
-    total = transformed.reduce((a, b) => a + b, 0);
-    console.log('Total weight of fuel needed (withAddedFuel):', total);
-});
+transformed = modules.map(calculateWeightOfFuel);
+total = transformed.reduce((a, b) => a + b, 0);
+console.log('Total weight of fuel needed (with added fuel):', total);
