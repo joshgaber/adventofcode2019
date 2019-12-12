24ef3d1daed9ec3b561b80c3737f92a6b862a0d6
Josh Gaber <joshgaber@gmail.com>
2019-12-12 17:00:37 -0500

    Synchronous file read. Can be run from anywhere.

--- day2/index.js
+++ day2/index.js
@@ -1,23 +1,23 @@
 const fs = require('fs');
+const path = require('path');
 const Intcode = require('./intcode');
 const goal = 19690720;
 
-fs.readFile('./input.txt', 'utf8', function(err, data) {
-    const memory = data.split(',').map(Number);
+const data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
+const memory = data.split(',').map(Number);
 
-    const test = new Intcode(memory.slice(), 12, 2);
-    test.run();
+const test = new Intcode(memory.slice(), 12, 2);
+test.run();
 
-    console.log('Gravity assist reset code:', test.output);
+console.log('Gravity assist reset code:', test.output);
 
-    for(let i = 0; i < 100; i++) {
-        for (let j = 0; j < 100; j++) {
-            const test = new Intcode(memory.slice(), i, j);
-            test.run();
+for(let i = 0; i < 100; i++) {
+    for (let j = 0; j < 100; j++) {
+        const test = new Intcode(memory.slice(), i, j);
+        test.run();
 
-            if (test.output === goal) {
-                console.log(`noun=${i} verb=${j}; input:`, i * 100 + j);
-            }
+        if (test.output === goal) {
+            console.log(`noun=${i} verb=${j}; input:`, i * 100 + j);
         }
     }
-});
+}
