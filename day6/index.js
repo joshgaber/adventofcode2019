9a65dd399ab2b8f651004175edf1818ad1951398
Josh Gaber <joshgaber@gmail.com>
2019-12-12 20:10:24 -0500

    Synchronous file read. Can be run from anywhere.

--- day6/index.js
+++ day6/index.js
@@ -1,36 +1,9 @@
 const fs = require('fs');
-let explored = [];
+const path = require('path');
 
-fs.readFile('./input.txt', 'utf8', function(err, data) {
-    const orbits = data.split(/\r?\n/g);
-    let orbitMap = [];
+const getChildrenDepth = o => o.children.reduce((a, b) => a + getChildrenDepth(b), o.children.length);
 
-    orbits.forEach(function(orbit) {
-        const [orbitee, orbiter] = orbit.split(')');
-        if (! ($orbiter = orbitMap.find(em => orbiter === em.name))) {
-            $orbiter = { name: orbiter, neighbors: [], children: [], parent: null };
-            orbitMap.push($orbiter);
-        }
-        if (! ($orbitee = orbitMap.find(em => orbitee === em.name))) {
-            $orbitee = { name: orbitee, neighbors: [], children: [], parent: null };
-            orbitMap.push($orbitee);
-        }
-        $orbitee.neighbors.push($orbiter);
-        $orbitee.children.push($orbiter);
-        $orbiter.neighbors.push($orbitee);
-        $orbiter.parent = $orbitee;
-    });
-
-    const totalOrbits = orbitMap.reduce((a, b) => a + getChildrenDepth(b), 0);
-    console.log(`Total direct and indirect orbits:`, totalOrbits);
-
-    let path = findPath(orbitMap.find(o => o.name === 'SAN').parent, orbitMap.find(o => o.name === 'YOU').parent);
-    let distance = path.match(/:/g).length;
-
-    console.log(`Number of hops from YOU parent to SAN parent:`, distance);
-});
-
-function findPath(target, orbit) {
+function findPath(target, orbit, explored = []) {
     explored.push(orbit.name);
 
     if (orbit.name === target.name) {
@@ -38,11 +11,35 @@ function findPath(target, orbit) {
     }
 
     let findNext = orbit.neighbors.reduce((path, o) =>
-        path + (!explored.includes(o.name) ? findPath(target, o) : '')
-    , '');
+        path + (!explored.includes(o.name) ? findPath(target, o, explored) : '')
+        , '');
     return (findNext !== '' ? findNext + ':' + orbit.name : '');
 }
 
-function getChildrenDepth(o) {
-    return o.children.reduce((a, b) => a + getChildrenDepth(b), o.children.length);
-}
+const data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
+const orbits = data.split(/\r?\n/g);
+const orbitMap = [];
+
+orbits.forEach(function(orbit) {
+    const [orbitee, orbiter] = orbit.split(')');
+    if (! ($orbiter = orbitMap.find(em => orbiter === em.name))) {
+        $orbiter = { name: orbiter, neighbors: [], children: [], parent: null };
+        orbitMap.push($orbiter);
+    }
+    if (! ($orbitee = orbitMap.find(em => orbitee === em.name))) {
+        $orbitee = { name: orbitee, neighbors: [], children: [], parent: null };
+        orbitMap.push($orbitee);
+    }
+    $orbitee.neighbors.push($orbiter);
+    $orbitee.children.push($orbiter);
+    $orbiter.neighbors.push($orbitee);
+    $orbiter.parent = $orbitee;
+});
+
+const totalOrbits = orbitMap.reduce((a, b) => a + getChildrenDepth(b), 0);
+console.log(`Total direct and indirect orbits:`, totalOrbits);
+
+const orbitPath = findPath(orbitMap.find(o => o.name === 'SAN').parent, orbitMap.find(o => o.name === 'YOU').parent);
+const distance = orbitPath.match(/:/g).length;
+
+console.log(`Number of hops from YOU parent to SAN parent:`, distance);
