a434794b0dd079531b79866d9f4d77b2ecbba091
Josh Gaber <joshgaber@gmail.com>
2019-12-12 20:10:08 -0500

    Synchronous file read. Can be run from anywhere.

--- day5/index.js
+++ day5/index.js
@@ -1,16 +1,16 @@
 const fs = require('fs');
+const path = require('path');
 const Intcode = require('./intcode');
 
-fs.readFile('./input.txt', 'utf8', function(err, data) {
-    const memory = data.split(',').map(data => parseInt(data));
+const data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
+const memory = data.split(',').map(data => parseInt(data));
 
-    let machine = new Intcode(memory.slice(), 1);
-    machine.run();
+let machine = new Intcode(memory.slice(), 1);
+machine.run();
 
-    console.log(`diagnostic code for ID 1:`, machine.output);
+console.log(`diagnostic code for ID 1:`, machine.output);
 
-    machine = new Intcode(memory.slice(), 5);
-    machine.run();
+machine = new Intcode(memory.slice(), 5);
+machine.run();
 
-    console.log(`diagnostic code for ID 5:`, machine.output);
-});
+console.log(`diagnostic code for ID 5:`, machine.output);
