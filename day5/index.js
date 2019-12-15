ed6301913af50f394ddb741c2f2ddc37b6db20de
Josh Gaber <joshgaber@gmail.com>
2019-12-15 15:31:30 -0500

    Uses universal Intcode machine

--- day5/index.js
+++ day5/index.js
@@ -1,16 +1,16 @@
 const fs = require('fs');
 const path = require('path');
-const Intcode = require('./intcode');
+const Intcode = require('../utilities/intcode');
 
 const data = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
 const memory = data.split(',').map(data => parseInt(data));
 
-let machine = new Intcode(memory.slice(), 1);
-machine.run();
+let machine = new Intcode(memory.slice());
+machine.pushInputs([1]).run();
 
-console.log(`diagnostic code for ID 1:`, machine.output);
+console.log(`diagnostic code for ID 1:`, machine.lastOutput);
 
-machine = new Intcode(memory.slice(), 5);
-machine.run();
+machine = new Intcode(memory.slice());
+machine.pushInputs([5]).run();
 
-console.log(`diagnostic code for ID 5:`, machine.output);
+console.log(`diagnostic code for ID 5:`, machine.lastOutput);
